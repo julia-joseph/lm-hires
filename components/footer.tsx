4dedc9e42b18a31413053547effcd906668f35c2
julia-joseph <juljoe726@gmail.com>
2026-03-15 07:41:56 +0530

    changes

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@
 export function Footer() {
   return (
     <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs h-10">
-      @ {new Date().getFullYear()} Lingomentor GmbH. All rights reserved.
+      @ 2026 Lingomentor GmbH. All rights reserved.
     </footer>
   );
 }
